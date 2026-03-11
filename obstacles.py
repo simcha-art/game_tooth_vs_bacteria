f75298d0522eea4546495d137f719fe2dd739b98
נתנאל קאפלין <kaplinnetanel10@gmail.com>
2026-03-11 22:34:53 +0200

    Gives an option to the voice that is an obstacle in life to remove

--- obstacles.py
+++ obstacles.py
@@ -13,3 +13,24 @@ class Candy:
 
     def draw(self, offset_x):
         screen.blit(candy_img,(self.rect.x - offset_x, self.rect.y))
+
+# ---------------- COLA ----------------
+class Cola:
+    def __init__(self):
+        # המיקום: 520 כדי שיהיה מתחת לרצפה (שהיא בדרך כלל ב-500)
+        self.rect = pygame.Rect(350, 520, 120, 80)
+        self.color = (60, 30, 0) # צבע חום קולה
+
+    def update(self, player, game_state):
+        # בדיקת התנגשות עם השחקן שמישהו אחר יצר
+        if player.rect.colliderect(self.rect):
+            # מוריד ניקוד במקום להרוג (hp=0)
+            if game_state['score'] > 0:
+                game_state['score'] -= 1
+            player.speed = 1  # האטה בגלל הדביקות
+        else:
+            player.speed = 5  # מהירות רגילה מחוץ לבור
+
+    def draw(self, screen, offset_x):
+        # כאן קורה הקסם של הציור עם ה-offset
+        pygame.draw.rect(screen, self.color, (self.rect.x - offset_x, self.rect.y, self.rect.width, self.rect.height))
